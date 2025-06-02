b0a327c32f5f3402d2b35348a5f4cdc192e0c635
Alexey Basinov <abasinov@google.com>
2025-06-02 09:38:39 -0700

    Add presubmit test for single-instance deployment on GCP

---
+++ presubmit_tests/single-instance-on-gcp.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+gcs_bucket="gs://oracle-toolkit-presubmit-artifacts"
+# Append BUILD_ID to the file name to ensure each zip file gets a unique name.
+# This prevents one test from deleting the file while it's still in use by another concurrently running test.
+# For available Prow-injected environment variables, see:
+# https://docs.prow.k8s.io/docs/jobs/#job-environment-variables
+toolkit_zip_file_name="oracle-toolkit-${BUILD_ID}.zip"
+tfvars_file="./presubmit_tests/single-instance.tfvars"
+instance_name="github-presubmit-si-${BUILD_ID}"
+deployment_name="presubmit-si-${BUILD_ID}"
+gcs_source="${gcs_bucket}/${toolkit_zip_file_name}"
+deployment_id="projects/gcp-oracle-benchmarks/locations/us-central1/deployments/${deployment_name}"
+
+apk add --no-cache zip jq
+
+echo "Zipping CWD into /tmp/${toolkit_zip_file_name} and uploading to ${gcs_bucket}/..."
+zip -r /tmp/${toolkit_zip_file_name} . -x ".git*" -x ".terraform*" -x "terraform*" -x OWNERS > /dev/null
+gcloud storage cp /tmp/${toolkit_zip_file_name} "${gcs_bucket}/"
+
+sed -i "s|@deployment_name@|$deployment_name|g" "${tfvars_file}"
+sed -i "s|@gcs_source@|$gcs_source|g" "${tfvars_file}"
+sed -i "s|@instance_name@|$instance_name|g" "${tfvars_file}"
+
+echo "Applying Infra Manager deployment: ${deployment_id}"
+
+gcloud infra-manager deployments apply "${deployment_id}" \
+  --service-account="projects/gcp-oracle-benchmarks/serviceAccounts/infra-manager-deployer@gcp-oracle-benchmarks.iam.gserviceaccount.com" \
+  --local-source="./terraform" \
+  --inputs-file="${tfvars_file}"
+
+cleanup() {
+  echo "Cleaning up: deleting "${gcs_source}" GCS object and ${deployment_id} Infra Manager deployment..."
+  if gcloud infra-manager deployments describe "${deployment_id}" >/dev/null 2>&1; then
+    gcloud --quiet infra-manager deployments delete "${deployment_id}" 
+  fi
+  if gcloud storage objects describe "${gcs_source}" >/dev/null 2>&1; then
+    gcloud --quiet storage rm "${gcs_source}"
+  fi
+}
+
+trap cleanup SIGINT SIGTERM EXIT
+
+SLEEP_SECONDS=60
+TIMEOUT_SECONDS=7200
+START_TIME=$(date +%s)
+
+echo "Waiting up to 2 hours for control node's startup script to complete..."
+
+while true; do
+  CURRENT_TIME=$(date +%s)
+  ELAPSED=$((CURRENT_TIME - START_TIME))
+
+  if [[ "${ELAPSED}" -ge "${TIMEOUT_SECONDS}" ]]; then
+    echo "Timeout reached after 2 hours. No completion log found."
+    exit 1
+  fi
+
+  result=$(gcloud logging read \
+    "resource.type=global AND \
+    log_name=projects/gcp-oracle-benchmarks/logs/Ansible_logs AND \
+    jsonPayload.deployment_name=${deployment_name} AND \
+    jsonPayload.event_type=ANSIBLE_RUNNER_SCRIPT_END" \
+    --order=desc \
+    --limit=1 \
+    --format=json)
+
+  state=$(echo "${result}" | jq -r '.[0].jsonPayload.state // empty')
+
+  if [[ "${state}" == "ansible_completed_success" ]]; then
+    echo "Control node's startup script completed successfully."
+    exit 0
+  elif [[ "${state}" == "ansible_completed_failure" ]]; then
+    echo "Control node's startup script failed."
+    exit 1
+  fi
+
+  sleep "${SLEEP_SECONDS}"
+done
---
+++ presubmit_tests/single-instance.tfvars
@@ -0,0 +1,56 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# See variables.tf for a full list, and descriptions.
+
+# Placeholder values in the form "@value@" will be replaced with actual values by the single-instance-on-gcp.sh script.
+gcs_source = "@gcs_source@"
+deployment_name = "@deployment_name@"
+instance_name = "@instance_name@"
+
+ora_swlib_bucket = "gs://bmaas-testing-oracle-software"
+project_id = "gcp-oracle-benchmarks"
+vm_service_account = "oracle-vm-runner@gcp-oracle-benchmarks.iam.gserviceaccount.com"
+control_node_service_account = "control-node-sa@gcp-oracle-benchmarks.iam.gserviceaccount.com"
+install_workload_agent = true
+oracle_metrics_secret = "projects/gcp-oracle-benchmarks/secrets/workload-agent-user-password/versions/latest"
+db_password_secret = "projects/gcp-oracle-benchmarks/secrets/sys-user-password/versions/latest"
+control_node_name_prefix="github-presubmit-control-node"
+source_image_family = "oracle-linux-8"
+source_image_project = "oracle-linux-cloud"
+machine_type = "n4-standard-2"
+boot_disk_type = "hyperdisk-balanced"
+boot_disk_size_gb = "50"
+swap_disk_size_gb = "50"
+zone1 = "us-central1-b"
+subnetwork1 = "projects/gcp-oracle-benchmarks/regions/us-central1/subnetworks/github-presubmit-tests-us-central1"
+oracle_home_disk = {
+  size_gb = 50
+}
+data_disk = {
+  size_gb = 50
+}
+reco_disk = {
+  size_gb = 50
+}
+ora_version = "19"
+ora_release = "latest"
+ora_edition = "EE"
+ora_backup_dest = "+RECO"
+ora_db_name = "orcl"
+ora_db_container = false
+ntp_pref = "169.254.169.254"
+ora_listener_port = "1521"
+ora_redo_log_size = "100MB"
+skip_database_config = false
