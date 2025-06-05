b19d2db24df4e947079f89b4e31a31067f8643f5
Alexey Basinov <abasinov@google.com>
2025-06-05 11:09:36 -0700

    update required version and add random provider

--- terraform/modules/oracle_toolkit_module/versions.tf
+++ terraform/modules/oracle_toolkit_module/versions.tf
@@ -13,11 +13,15 @@
 # limitations under the License.
 
 terraform {
-  required_version = ">= 1.10.5"
+  required_version = ">= 1.5.7"
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = "~> 6.20.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.7.2"
     }
   }
 }
